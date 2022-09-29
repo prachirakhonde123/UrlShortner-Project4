b4fe4710bab60c10e7dbabc564e9fdbf10347db7
Prachi Rakhonde <prachirakhonde2000@gmail.com>
2022-09-29 16:31:33 +0530

    2 Api's Done

--- src/Controllers/urlController.js
+++ src/Controllers/urlController.js
@@ -3,39 +3,77 @@ const validurl = require('valid-url')
 const shortid = require('shortid')
 //const config = require('config')
 const baseURL = "http://localhost:3000"
+const { isValid, isValidBody } = require('../validator/validator')
 
 
-const createURL = async function(req,res){
-    try{
+const createURL = async function (req, res) {
+    try {
         const data = req.body
-        const longUrl = req.body.longUrl
+        const { longUrl } = data
+
+        if (!isValidBody(data)) {
+            return res.status(400).send({ status: true, message: "Body can't be empty" })
+        }
+
+        if (!longUrl) {
+            return res.status(400).send({ status: false, message: "Please Provide LongURL" })
+        }
+
+        if (Object.keys(data).length !== 1) {
+            return res.status(400).send({ status: false, message: "Body can contain only one key and i.e longUrl" })
+        }
+
+        if (longUrl) {
+            if (!isValid(longUrl)) return res.status(400).send({ status: false, message: "Please Provide Valid LongUrl" })
+
+            if (!validurl.isWebUri(longUrl)) return res.status(400).send({ status: false, message: "Your Url is Invalid. Please Provide Valid LongUrl" })
+        }
+
+        const duplicateUrl = await urlModel.findOne({ longUrl: longUrl })
+        if (duplicateUrl) {
+            return res.status(400).send({ status: true, message: "LongUrl is already Used" })
+        }
 
         const codeUrl = shortid.generate(longUrl).toLowerCase()  // shortcode is generated
         //console.log(code)
         const shortUrl = baseURL + "/" + codeUrl     // shortUrl created
         //console.log(shortUrl)
 
+        const duplicateShortUrl = await urlModel.findOne({ shortUrl: shortUrl })
+        if (duplicateShortUrl) {
+            return res.status(400).send({ status: false, message: "ShortUrl is already exist !" })
+        }
+
         data["urlCode"] = codeUrl
         data["shortUrl"] = shortUrl
         //console.log(data)
 
         const createUrl = await urlModel.create(data)
-        res.send({status : true, data : createUrl})
+        res.send({ status: true, data: data })
     }
 
-    catch(err){
-        res.status(500).send({status : false, message : err.message})
+    catch (err) {
+        res.status(500).send({ status: false, message: err.message })
     }
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const getUrl = async function(req,res){
-    const getUrlList = await urlModel.find().select({_id : 0, longUrl : 1, shortUrl : 1, urlCode : 1})
-    res.status(200).send({status : true, data : getUrlList})
+const getUrl = async function (req, res) {
+    try {
+        const urlcode = req.params.urlCode
+        const getUrlList = await urlModel.findOne({ urlCode: urlcode }).select({ _id: 0, longUrl: 1, shortUrl: 1, urlCode: 1 })
+        if (!getUrlList) {
+            return res.status(404).send({status : false, message : "No data found with this urlCode!"})
+        }
+        return res.status(302).redirect(getUrlList.longUrl)
+    }
+    catch (err) {
+        res.status(500).send({ status: false, message: "Server Error", error: err.message })
+    }
 }
 
 
-module.exports = {createURL, getUrl}
+module.exports = { createURL, getUrl }
 
--- src/route/route.js
+++ src/route/route.js
@@ -7,8 +7,8 @@ router.get("/test-me", function(req,res){
     res.send("This is first api!")
 })
 
-router.post("/Url", createUrl.createURL)
-router.get("/Url", createUrl.getUrl)
+router.post("/url/shorten", createUrl.createURL)
+router.get("/:urlCode", createUrl.getUrl)
 
 //======================== to check if the endpoint is correct or not =========================================
 router.all("/**", function (req, res) {
